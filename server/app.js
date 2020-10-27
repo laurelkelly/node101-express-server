b18588a908503ec2846201220a8d95adf8086e94
Laurel Kelly <laurellkelly@gmail.com>
2020-10-27 14:45:24 -0700

    Commit all the files

--- server/app.js
+++ server/app.js
@@ -1,10 +1,27 @@
 // import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const topSpotsData = require('./data.json');
 
+var bodyParser = require('body-parser');
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.status(200).send("It's working");
+});
+
+app.get('/data', function(req, res) {
+    res.json(topSpotsData);
+});
 
 // finally export the express application
 module.exports = app;
